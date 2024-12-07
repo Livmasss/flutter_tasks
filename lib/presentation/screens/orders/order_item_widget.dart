fca73d6f8b2c955a3fa4c091a361cad832c9726e
livmas <seiranyan.sg@gmail.com>
2024-12-07 16:38:27 +0300

    OrderItemWidget.dart created

---
+++ lib/presentation/screens/orders/order_item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../models/order_model.dart';
+
+class OrderItemWidget extends StatelessWidget {
+  final OrderModel order;
+  
+  const OrderItemWidget({
+    super.key,
+    required this.order
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Заказ номер: ${order.id}"),
+                  const SizedBox(height: 4),
+                  Text("На сумму ${order.total} \$"),
+                ]
+            ),
+            const Spacer(),
+            Text("Статус:\n${order.status}")
+        ],
+      )
+    );
+  }
+}
--- lib/presentation/screens/orders/orders_screen.dart
+++ lib/presentation/screens/orders/orders_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_task3/data/orders_service.dart';
 
 import '../../models/order_model.dart';
+import 'order_item_widget.dart';
 
 class OrdersScreen extends StatefulWidget {
   const OrdersScreen({super.key});
@@ -27,7 +28,12 @@ class _OrdersScreenState extends State<OrdersScreen> {
         child: Scaffold(
           body: ListView.builder(
             itemCount: orders.length,
-              itemBuilder: (BuildContext context, int index) => Text(orders[index].total.toString())
+              itemBuilder: (BuildContext context, int index) => Column(
+                children: [
+                  OrderItemWidget(order: orders[index]),
+                  const Divider()
+                ],
+              )
             )
         )
     );
